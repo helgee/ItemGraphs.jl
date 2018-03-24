0bc04f0d66a55288bf3eaa90254220f90e8ee944
autodocs <autodocs>
2018-03-24 17:13:00 +0000

    build based on 6754608

--- latest/search_index.js
+++ latest/search_index.js
@@ -44,31 +44,47 @@ var documenterSearchIndex = {"docs": [
     "location": "api.html#ItemGraphs.ItemGraph",
     "page": "API",
     "title": "ItemGraphs.ItemGraph",
-    "category": "Type",
-    "text": "ItemGraph{T}([graph]; lazy=false)\n\nCreate a new ItemGraph with items of type T based on a graph from LightGraphs. graph can be omitted and a DiGraph will be used by default. If lazy is set to true, the paths between items will be computed on-demand when getpath is called. Otherwise all paths recomputed whenever a new edge is inserted.\n\n\n\n\n\n"
+    "category": "type",
+    "text": "ItemGraph{T,S}([graph]; lazy=false)\n\nCreate a new ItemGraph with items of type T based on a graph from LightGraphs. Items of type S can be assigned to the edges of the graph, this type can be omitted and will be Float64 by default. graph can be omitted as well and a DiGraph will be used by default. If lazy is set to true, the paths between items will be computed on-demand when items is called. Otherwise all paths are recomputed whenever a new edge is inserted.\n\n\n\n\n\n"
 },
 
 {
-    "location": "api.html#ItemGraphs.getpath-Tuple{Any,Any,Any}",
+    "location": "api.html#ItemGraphs.edgeitem-Union{Tuple{T}, Tuple{ItemGraph{T,S,G} where G where S,T,T}} where T",
     "page": "API",
-    "title": "ItemGraphs.getpath",
-    "category": "Method",
-    "text": "getpath(graph, from, to)\n\nGet the path between the items from and to. Will throw an ItemGraphsException if either from or to are not a part of graph or if there is no path between them.\n\n\n\n\n\n"
+    "title": "ItemGraphs.edgeitem",
+    "category": "method",
+    "text": "edgeitem(graph, from, to)\n\nGet the item assigned to the edge between from and to.\n\n\n\n\n\n"
 },
 
 {
-    "location": "api.html#LightGraphs.add_edge!-Union{Tuple{T}, Tuple{ItemGraph{T,G} where G,T,T}} where T",
+    "location": "api.html#ItemGraphs.edgeitems-Union{Tuple{S}, Tuple{T}, Tuple{ItemGraph{T,S,G} where G,T,T}} where S where T",
     "page": "API",
-    "title": "LightGraphs.add_edge!",
-    "category": "Method",
-    "text": "add_edge!(graph, from, to)\n\nAdd an edge between from and to to graph.\n\n\n\n\n\n"
+    "title": "ItemGraphs.edgeitems",
+    "category": "method",
+    "text": "edgeitems(graph, from, to)\n\nGet all items assigned to the edges between from and to.\n\n\n\n\n\n"
 },
 
 {
-    "location": "api.html#LightGraphs.add_vertex!-Union{Tuple{T}, Tuple{ItemGraph{T,G} where G,T}} where T",
+    "location": "api.html#ItemGraphs.items-Tuple{Any,Any,Any}",
     "page": "API",
-    "title": "LightGraphs.add_vertex!",
-    "category": "Method",
+    "title": "ItemGraphs.items",
+    "category": "method",
+    "text": "items(graph, from, to)\n\nGet the items on the path between and including from and to. Will throw an ItemGraphsException if either from or to are not a part of graph or if there is no path between them.\n\n\n\n\n\n"
+},
+
+{
+    "location": "api.html#LightGraphs.SimpleGraphs.add_edge!-Union{Tuple{S}, Tuple{T}, Tuple{ItemGraph{T,S,G} where G,T,T}, Tuple{ItemGraph{T,S,G} where G,T,T,S}} where S where T",
+    "page": "API",
+    "title": "LightGraphs.SimpleGraphs.add_edge!",
+    "category": "method",
+    "text": "add_edge!(graph, from, to, [item])\n\nAdd an edge between from and to to graph. Optionally assign an item to the edge.\n\n\n\n\n\n"
+},
+
+{
+    "location": "api.html#LightGraphs.SimpleGraphs.add_vertex!-Union{Tuple{T}, Tuple{ItemGraph{T,S,G} where G where S,T}} where T",
+    "page": "API",
+    "title": "LightGraphs.SimpleGraphs.add_vertex!",
+    "category": "method",
     "text": "add_vertex!(graph, item)\n\nAdd item to graph.\n\n\n\n\n\n"
 },
 
@@ -76,7 +92,7 @@ var documenterSearchIndex = {"docs": [
     "location": "api.html#LightGraphs.has_path-Tuple{ItemGraph,Any,Any}",
     "page": "API",
     "title": "LightGraphs.has_path",
-    "category": "Method",
+    "category": "method",
     "text": "has_path(graph, from, to)\n\nReturn true if there is a path between from and to in graph.\n\n\n\n\n\n"
 },
 
@@ -84,7 +100,7 @@ var documenterSearchIndex = {"docs": [
     "location": "api.html#LightGraphs.has_vertex-Tuple{ItemGraph,Any}",
     "page": "API",
     "title": "LightGraphs.has_vertex",
-    "category": "Method",
+    "category": "method",
     "text": "has_vertex(graph, item)\n\nReturn true if item is contained in graph.\n\n\n\n\n\n"
 },
 
